54f2c47563cc6145a3f322710f6be2960eeb3b3b
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-30 17:29:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,10 @@ def hit? (current_total)
   prompt_user
   user_input = get_user_input
   if user_input == 's'
-    display_card_total(current_total)
+    #display_card_total(current_total)
   elsif user_input == 'h'
     current_total += deal_card
-    display_card_total(current_total)
+    #display_card_total(current_total)
   else
     invalid_command
     prompt_user
